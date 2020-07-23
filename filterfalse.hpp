d4fe5bbb77ad67b0c5ff75350df629fda0030402
Yarden Cohen <57362284+yarden7696@users.noreply.github.com>
2020-07-23 20:21:34 +0300

    Update filterfalse.hpp

--- filterfalse.hpp
+++ filterfalse.hpp
@@ -5,58 +5,58 @@
 
 namespace itertools{
 
-    template<typename sign, typename container>
+    template <typename sign, typename container> class filterfalse {
 
-    class filterfalse {
-
-        sign _s;
-        container _container;
-        typedef typename container::value_type value_type;
+        sign _s; // The condition on every element in the container, whoever **dont** answer the condition will enter the result container
+        container _container;  // the container that we get(could be range/ vector/ array...)
+        typedef typename container::value_type value_type; // the type of the elements that in the container
 
     public:
+        //  filterfalse CONSTRUCTOR
         filterfalse(sign s, container cont): _s(s), _container(cont){}
 
-        class iterator{
-            typename container::iterator _begin;
-            typename container::iterator _end;
-            sign _s;
+        class iterator {
+            
+            typename container::iterator _begin; //we take from the container the begin iterator(for example-if the container is range so _begin is num1)
+            typename container::iterator _end; //we take from the container the end iterator(for example-id the container is range so _iter is num2)
+            sign _s; // the condition on the container's elements
 
         public:
-            iterator(typename container::iterator iter, typename container::iterator end, sign s) : _begin(iter), _end(end), _s(s){
-                while (_begin != _end && _s(*_begin))
+            //  iterator CONSTRUCTOR
+            iterator(typename container::iterator begin, typename container::iterator end, sign s) : _begin(begin), _end(end), _s(s){
+                while (_begin != _end && _s(*_begin)) //In this while loop we looking for the first element that return false to the condition
                     ++_begin;
             }
 
-            iterator& operator=(const iterator& it){
-                if(this != &it) {
-                    this->_begin = it._begin;
-                    this->_end = it._end;
-                    this->_s = it._s;
+            iterator& operator=(const iterator& other){
+                if(this != &other) {
+                    this->_begin = other._begin;
+                    this->_end = other._end;
+                    this->_s = other._s;
                 }
+                return *this; // init this itarator with the values of "other itarator" and return *this 
+            }
+
+
+            bool operator==(const iterator& other) { // checks if other and this points to the same place
+                return _begin == other._begin;
+            }
+
+            bool operator!=(const iterator& other) { // checks if other and this points to the diffrent place
+                return _begin != other._begin;
+            }
+
+            iterator& operator++() {
+                do { ++_begin; } 
+                while (_begin != _end && _s(*_begin));
                 return *this;
             }
 
-
-            bool operator==(const iterator& it) {
-                return _begin == it._begin;
-            }
-
-            bool operator!=(const iterator& it) {
-                return _begin != it._begin;
-            }
-
-            iterator& operator++(){
-                do{
-                    ++_begin;
-                } while (_begin != _end && _s(*_begin));
-                return *this;
-            }
-
-            iterator operator++(int){
-                iterator iter = *this;
-                ++(*this);
-                return iter;
-            }
+//             iterator operator++(int){
+//                 iterator iter = *this;
+//                 ++(*this);
+//                 return iter;
+//             }
 
             value_type operator*(){
                 return *_begin;
